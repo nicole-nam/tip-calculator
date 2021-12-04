c6257129f48f121db2eda25196966c4789d525a8
nicole-nam <74622899+nicole-nam@users.noreply.github.com>
2021-12-03 19:51:37 -0500

    uiux: form styling add

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,22 +1,35 @@
 <template>
   <div>Tip Calculator</div>
   <div class="card">
-    <label>Bill</label>
-    <br />
-    <input type="number" />
-    <br />
-    <label>Select Tip %</label>
-    <div class="grid">
-      <input type="button" value="5%" />
-      <input type="button" value="10%" />
-      <input type="button" value="15%" />
-      <input type="button" value="25%" />
-      <input type="button" value="50%" />
-      <input type="button" value="Custom" />
+    <div class="form-card">
+      <label>Bill</label>
+      <br />
+      <input type="number" />
+      <br />
+      <label>Select Tip %</label>
+      <div class="grid">
+        <input type="button" value="5%" />
+        <input type="button" value="10%" />
+        <input type="button" value="15%" />
+        <input type="button" value="25%" />
+        <input type="button" value="50%" />
+        <input type="button" value="Custom" />
+      </div>
+      <br />
+      <label>Number of People</label>
+      <input type="number" />
     </div>
-    <br />
-    <label>Number of People</label>
-    <input type="number" />
+    <div class="tip-card">
+      <div class="margin-bottom">
+        <p>Tip Amount</p>
+        <p>/person</p>
+      </div>
+      <div>
+        <p>Total</p>
+        <p>/person</p>
+      </div>
+      <button class="resetBtn">Reset</button>
+    </div>
   </div>
 </template>
 
@@ -33,10 +46,53 @@ export default {
   border-radius: 5px;
   width: 40%;
   margin: 100px auto;
+  padding: 20px;
+  text-align: left;
+  display: flex;
+  gap: 30px;
 }
+.tip-card {
+  background-color: hsl(183, 100%, 15%);
+  border-radius: 5px;
+  width: 40%;
+  margin: auto;
+  padding: 20px;
+  text-align: left;
+}
+
+p {
+  color: #fff;
+  margin: 0;
+}
+
+.margin-bottom {
+  margin-bottom: 50px;
+}
+
 .grid {
   display: grid;
   grid-gap: 1px;
   grid-template-columns: repeat(3, 1fr);
+}
+
+.resetBtn {
+  width: 100%;
+  margin-top: 20px;
+  background-color: hsl(186, 42%, 59%);
+  border: hsl(186, 42%, 59%);
+  border-radius: 3px;
+  color: hsl(183, 100%, 15%);
+}
+
+input[type="button"] {
+  background-color: hsl(183, 100%, 15%);
+  border: hsl(183, 100%, 15%);
+  border-radius: 3px;
+  color: #fff;
+  padding: 5px 10px;
+}
+
+input[type="number"] {
+  margin-bottom: 20px;
 }
 </style>
